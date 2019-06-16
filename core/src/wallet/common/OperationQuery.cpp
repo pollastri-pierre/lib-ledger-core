d32bdbd5321e139b74f4fe97c76fb0575dc53444
Pierre Pollastri <pierre@ledger.fr>
2019-06-16 15:29:33 +0200

    [cosmos] fix bad include

--- core/src/wallet/common/OperationQuery.cpp
+++ core/src/wallet/common/OperationQuery.cpp
@@ -31,6 +31,7 @@
 #include "OperationQuery.h"
 #include <api/OperationListCallback.hpp>
 #include "Operation.h"
+#include <api/ErrorCode.hpp>
 #include <database/soci-date.h>
 #include <database/soci-option.h>
 #include <database/soci-number.h>
